41ee2bfadde31a073e6509152267d7e5b1961b8d
Cássio Morais <cassiomoraisg@hotmail.com>
2023-05-21 20:21:30 -0300

    - add documentation example

--- src/Api.Example.With.Many.Features.Rest/Controllers/v1/TestController.cs
+++ src/Api.Example.With.Many.Features.Rest/Controllers/v1/TestController.cs
@@ -11,6 +11,11 @@ namespace Api.Example.With.Many.Features.Rest.Controllers.v1
         [HttpGet("something")]
         public IActionResult Get() => Ok(new { test = "v1" });
 
+        /// <summary>
+        /// process data input
+        /// </summary>
+        /// <param name="data">just strings</param>
+        /// <response code="200">just the same data input</response>
         [HttpPost("something/data")]
         [ProducesResponseType(typeof(Data), StatusCodes.Status200OK)]
         public IActionResult PostData(Data data) => Ok(data);
